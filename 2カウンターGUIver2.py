3e8ff4f2cff035f2ea388de38d0683116d0727d2
BonnetPonta <80739002+BonnetPonta@users.noreply.github.com>
2021-09-06 12:56:46 +0900

    修正

--- 2カウンターGUIver2.py
+++ 2カウンターGUIver2.py
@@ -1,56 +1,55 @@
+"""
+徘徊者カウンター
+"""
 import tkinter
 import tkinter.ttk
-##############################################################################################################################
-# 変数定義 -> 関数 -> クリックイベント -> 画面作成 -> マッピングGUI
-##############################################################################################################################
-# 徘徊者カウンター
-# 変数定義 //lblがname //lbl_が残り数 //Aをリストかしてる
-A = [0]*8
-lbl_ = [0]*8
+
+lblR = [0]*8  # 右側の残り数
 txtbox = [0]*8
-
-##############################################################################################################################
-# //クリックイベント//
-# ##############################################################################################################################
-# 徘徊者カウンター：click時のイベント関数
+txtboxV = [0]*8
 
 
-def btn_click_set():  # setのs
-    goukei = 0
-    for s in range(8):
-        A[s] = int(txtbox[s].get())
-        lbl_[s]["text"] = A[s]
+def btn_click_set():
+    total = 0
+    for i in range(8):
+        txtboxV[i] = int(txtbox[i].get())
+        lblR[i]["text"] = txtboxV[i]
         # 合計左側1度だけ実行
-        goukei = int(goukei) + int(A[s])
-        lbl_gou2["text"] = goukei
+        total += int(txtboxV[i])
+        lbl_totalL["text"] = total
         kousin()
 
 
 def kousin():
-    goukei = 0
-    for k in range(8):
-        goukei = int(goukei) + int(A[k])
-        lbl_gou["text"] = goukei
+    total = 0
+    for i in range(8):
+        total += int(txtboxV[i])
+        lbl_totalR["text"] = total
 
 
 def btn_click_min(i):
     def aaa():
-        if 0 < int(A[i]):
-            A[i] = int(A[i])-1
-            lbl_[i]["text"] = A[i]
+        if 0 < int(txtboxV[i]):
+            txtboxV[i] -= 1
+            lblR[i]["text"] = txtboxV[i]
             kousin()
     return aaa
 
 
 def btn_click_plus(i):
     def bbb():
-        A[i] = int(A[i])+1
-        lbl_[i]["text"] = A[i]
+        txtboxV[i] += 1
+        lblR[i]["text"] = txtboxV[i]
         kousin()
     return bbb
 
 
-##############################################################################################################################
+def key_down(e):  # キーボード対応//Enter -> set
+    key = e.keysym
+    if key == "Return":
+        btn_click_set()
+
+
 # //画面作成//
 tki = tkinter.Tk()
 tki.geometry("300x300+1160+60")
@@ -61,50 +60,36 @@ tki.resizable(False, False)
 
 ##################################################################################################
 # 作成 //徘徊者一覧
-name = ["神楽鈴", "ランナー", "徘徊女", "蜘蛛", "警鐘", "梅", "固定女", "憎悪"]
+names = ["神楽鈴", "ランナー", "徘徊女", "蜘蛛", "警鐘", "梅", "固定女", "憎悪"]
 name_pos = []
-for n in range(8):  # テキストボックスとボタンの方を一緒に実行しちゃうとTABキー押したとき面倒だから先にbox先作る
-    lbl = tkinter.Label(text=name[n], bg="#ffff00")
-    name_pos.append(20+n*30)
-    lbl.place(x=10, y=name_pos[n])
+for i in range(8):  # テキストボックスとボタンの方を一緒に実行しちゃうとTABキー押したとき面倒だから先にbox先作る
+    lblL = tkinter.Label(text=names[i], bg="#ffff00")
+    name_pos.append(20+i*30)
+    lblL.place(x=10, y=name_pos[i])
     # テキストボックスの作成
-    txtbox[n] = tkinter.Entry(width=20)
-    txtbox[n].place(x=60, y=name_pos[n], width=50)
+    txtbox[i] = tkinter.Entry(width=20)
+    txtbox[i].place(x=60, y=name_pos[i], width=50)
 
 for i in range(8):
-    # ラベル初期値
-    lbl_[i] = tkinter.Label(text="", bg="white")
-    lbl_[i].place(x=230, y=name_pos[i])
-    # ボタン作成
-    # -1
+    # 残り数ラベル初期値
+    lblR[i] = tkinter.Label(text="", bg="white")
+    lblR[i].place(x=230, y=name_pos[i])
+    # ±1ボタン作成
     btn = tkinter.Button(tki, text="-1", command=btn_click_min(i))
     btn.place(x=130, y=name_pos[i], width=50)
-    # +1
     btn = tkinter.Button(tki, text="+1", command=btn_click_plus(i))
     btn.place(x=250, y=name_pos[i], width=50)
 
 # 合計ラベルの作成
-lbl = tkinter.Label(text="#-合計-#", bg="#00ff00")
-lbl.place(x=10, y=260)
-lbl_gou2 = tkinter.Label(text="", bg="#00ff00")
-lbl_gou2.place(x=100, y=260)
-lbl_gou = tkinter.Label(text="", bg="#00ff00")
-lbl_gou.place(x=230, y=260)
-
-
-# キーボード対応//Enter -> set
-key = ""
-
-
-def key_down(e):
-    key = e.keysym
-    if key == "Return":
-        btn_click_set()
-
+lblL = tkinter.Label(text="#-合計-#", bg="#00ff00")
+lblL.place(x=10, y=260)
+lbl_totalL = tkinter.Label(text="", bg="#00ff00")
+lbl_totalL.place(x=100, y=260)
+lbl_totalR = tkinter.Label(text="", bg="#00ff00")
+lbl_totalR.place(x=230, y=260)
 
 # キー設定
 tki.bind("<KeyPress>", key_down)
-
 
 # 画面をそのまま表示
 tki.mainloop()
